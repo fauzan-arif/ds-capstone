ecfc5cf359afb0a6447925f422795b6ef7b70a76
Max Power <kevin.melchert@gmail.com>
2024-01-04 13:01:56 +0100

    app

--- app/web.py
+++ app/web.py
@@ -1,17 +1,37 @@
 from flask import Flask, render_template
+from flask_babel import Babel
+from urllib.parse import urlparse
 import yfinance as yf
 import pandas as pd
-from urllib.parse import urlparse
 
-app = Flask(__name__)
+from news_classifier import NewsClassifier
+
+app   = Flask(__name__)
+babel = Babel(app, default_locale='en')
 
 @app.route('/')
 def index():
     tickers = sorted(pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0].Symbol.to_list())
     return render_template('index.html', tickers=tickers)
     
+
 @app.route('/<symbol>')
 def ticker(symbol):
     ticker = yf.Ticker(symbol.upper())
     domain = urlparse(ticker.info['website']).hostname
-    return render_template('ticker.html', ticker=ticker, info=ticker.info, domain=domain)
+    ticker_news = news_with_sentiment(ticker.news)
+    
+    return render_template('ticker.html', ticker=ticker, info=ticker.info, domain=domain, ticker_news=ticker_news)
+    
+
+
+
+
+
+def news_with_sentiment(news):
+    nlp = NewsClassifier()
+    for i, article in enumerate(news):
+        news[i]['sentiment'] = nlp.sentiment_for(article['title'])
+
+    return news
+    
